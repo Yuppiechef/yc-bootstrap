5af453b75103eb04e2d5a119ac616c729cfb6292
Deon Moolman <deon@yuppiechef.com>
2022-02-26 23:32:25 +0200

    Basic HTTP Server, commit 2

--- src/start.clj
+++ src/start.clj
@@ -1,5 +1,14 @@
 (ns start
+  (:require
+   [nrepl.server :as nrepl]
+   [services.http :as http])
   (:gen-class))
 
+
 (defn -main [& args]
-  (println "Hello world"))
+  (println "Starting nrepl server")
+  (nrepl/start-server :port 7888)
+  (println "Starting HTTP server")
+  (http/start)
+  (.read (System/in))
+  (println "The end."))
